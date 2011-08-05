86e5043c260b3d713aee3147f4001f8cc20cfd26
Alexander Surma <alexander.surma@gmail.com>
2011-08-06 00:37:11 +0200

    Shell: Added isComment() tests

--- applets/shell/shell_test.go
+++ applets/shell/shell_test.go
@@ -23,15 +23,60 @@ func TestParameterizeStrings(t *testing.T) {
 	lines := map[string][]string{
 		"1 22 333":                      []string{"1", "22", "333"},
 		"\"111 11 11\" 222 \"333 333\"": []string{"111 11 11", "222", "333 333"},
-		"\"111 \\\" 111\" 222":          []string{"111 \" 111", "222"},
+		"\"111 \\\" 1\\11\" 222":        []string{"111 \" 1\\11", "222"},
 	}
 	for sin, sout := range lines {
-		params, e := parameterize(sin)
+		res, e := parameterize(sin)
 		if e != nil {
 			t.Errorf("Splitting <%s>: Expected %s, got an error: %s", sin, stringifyStringArray(sout), e.String())
 		}
-		if !compareStringArrays(sout, params) {
-			t.Errorf("Splitting <%s>: Expected %s, got %s", sin, stringifyStringArray(sout), stringifyStringArray(params))
+		if !compareStringArrays(sout, res) {
+			t.Errorf("Splitting <%s>: Expected %s, got %s", sin, stringifyStringArray(sout), stringifyStringArray(res))
+		}
+	}
+}
+
+func TestCleanMatch(t *testing.T) {
+	lines := map[string]string{
+		"abc":        "abc",
+		"a\\\"b":     "a\"b",
+		"a\\\\\\\"b": "a\\\"b",
+		"a\\\\b":     "a\\b",
+		"a\\t":       "a\\t",
+	}
+
+	for sin, sout := range lines {
+		res := cleanMatch(sin)
+		if res != sout {
+			t.Errorf("Cleaning <%s>: Expected <%s>, got <%s>", sin, sout, res)
+		}
+	}
+}
+
+func TestCommentRecognition(t *testing.T) {
+	positives := []string{
+		"#",
+		"#!/bin/bash",
+		"     #",
+		"\t     #",
+		"\t \t#",
+	}
+	for i, line := range positives {
+		if !isComment(line) {
+			t.Errorf("<%s> (#%d) not recognized as comment", line, i)
+		}
+	}
+
+	negatives := []string{
+		"asdf",
+		"    asd",
+		"        a#",
+		"      !#",
+		"     \a#",
+	}
+	for i, line := range negatives {
+		if isComment(line) {
+			t.Errorf("<%s> (#%d) recognized as comment", line, i)
 		}
 	}
 }
