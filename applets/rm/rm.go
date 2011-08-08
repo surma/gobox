c521cb5c831933a23f2cc73a5859a1e220f48a2d
Alexander Surma <alexander.surma@gmail.com>
2011-08-09 00:31:07 +0200

    Rm: Added recursive deleting

--- applets/rm/rm.go
+++ applets/rm/rm.go
@@ -2,14 +2,57 @@ package rm
 
 import (
 	"os"
+	"flag"
+	"io/ioutil"
+)
+
+var (
+	flagSet       = flag.NewFlagSet("rm", flag.PanicOnError)
+	recursiveFlag = flagSet.Bool("r", false, "Recurse into directories")
+	helpFlag      = flagSet.Bool("help", false, "Show this help")
 )
 
 func Rm(call []string) os.Error {
-	if len(call) <= 1 {
-		return os.NewError("`rm` <files...>")
+	e := flagSet.Parse(call[1:])
+	if e != nil {
+		return e
 	}
-	for _, file := range call[1:] {
-		e := os.Remove(file)
+	if flagSet.NArg() == 0 || *helpFlag {
+		println("`rm` [options] <files...>")
+		flagSet.PrintDefaults()
+		return nil
+	}
+
+	for _, file := range flagSet.Args() {
+		e := delete(file)
+		if e != nil {
+			return e
+		}
+	}
+	return nil
+}
+
+func delete(file string) os.Error {
+	fi, e := os.Stat(file)
+	if e != nil {
+		return e
+	}
+	if fi.IsDirectory() && *recursiveFlag {
+		e := deleteDir(file)
+		if e != nil {
+			return e
+		}
+	}
+	return os.Remove(file)
+}
+
+func deleteDir(dir string) os.Error {
+	files, e := ioutil.ReadDir(dir)
+	if e != nil {
+		return e
+	}
+	for _, file := range files {
+		e = delete(dir + "/" + file.Name)
 		if e != nil {
 			return e
 		}
