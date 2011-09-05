5a95750a895c5f522976cedaa856049adf038fa8
Alexander Surma <alexander.surma@gmail.com>
2011-09-06 00:55:58 +0200

    Kill: Added signal definitions

--- applets/kill/kill.go
+++ applets/kill/kill.go
@@ -21,6 +21,22 @@ func Kill(call []string) os.Error {
 	if flagSet.NArg() <= 0 || *helpFlag {
 		println("`kill` [options] <pid>")
 		flagSet.PrintDefaults()
+		println("1     SIGHUP       terminal line hangup")
+		println("2     SIGINT       interrupt program")
+		println("3     SIGQUIT      quit program")
+		println("4     SIGILL       illegal instruction")
+		println("5     SIGTRAP      trace trap")
+		println("6     SIGABRT      abort program (formerly SIGIOT)")
+		println("7     SIGEMT       emulate instruction executed")
+		println("8     SIGFPE       floating-point exception")
+		println("9     SIGKILL      kill program")
+		println("10    SIGBUS       bus error")
+		println("11    SIGSEGV      segmentation violation")
+		println("12    SIGSYS       non-existent system call invoked")
+		println("13    SIGPIPE      write on a pipe with no reader")
+		println("14    SIGALRM      real-time timer expired")
+		println("15    SIGTERM      software termination signal")
+		println("16    SIGURG       urgent condition present on socket")
 		return nil
 	}
 
