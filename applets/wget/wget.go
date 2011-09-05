9c70ecda5e44a62ff7b3f1c7172c3741982be99a
Alexander Surma <alexander.surma@gmail.com>
2011-09-05 23:56:21 +0200

    Wget: Added output flag

--- applets/wget/wget.go
+++ applets/wget/wget.go
@@ -11,6 +11,7 @@ import (
 
 var (
 	flagSet  = flag.NewFlagSet("wget", flag.PanicOnError)
+	outFlag = flagSet.String("o", "", "Filename to save output to")
 	helpFlag = flagSet.Bool("help", false, "Show this help")
 )
 
@@ -26,12 +27,7 @@ func Wget(call []string) os.Error {
 		return nil
 	}
 
-	filename, e := getFilenameFromURL(flagSet.Arg(0))
-	if e != nil {
-		return e
-	}
-
-	output, e := os.OpenFile(filename, os.O_CREATE|os.O_WRONLY, 0755)
+	output, e := getOutputFile(flagSet.Arg(0))
 	if e != nil {
 		return e
 	}
@@ -60,4 +56,22 @@ func getFilenameFromURL(rawurl string) (string, os.Error) {
 		fname = "index.html"
 	}
 	return fname, nil
+}
+
+func getOutputFile(rawurl string) (io.WriteCloser, os.Error) {
+	if *outFlag == "-" {
+		return os.Stdout, nil
+	}
+	var filename string
+	var e os.Error
+	if len(*outFlag) == 0 {
+		filename, e = getFilenameFromURL(flagSet.Arg(0))
+		if e != nil {
+			return nil, e
+		}
+	} else {
+		filename = *outFlag
+	}
+	return os.OpenFile(filename, os.O_CREATE|os.O_WRONLY, 0644)
+
 }
