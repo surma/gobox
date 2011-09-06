2764554bf551e8f848b0056ea318d473cedfc862
Alexander Surma <alexander.surma@gmail.com>
2011-09-07 00:02:18 +0200

    Mknod: Changed mode flag handling to octal

--- applets/mknod/mknod.go
+++ applets/mknod/mknod.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"syscall"
 	"strings"
+	"strconv"
 )
 
 var (
@@ -12,7 +13,7 @@ var (
 	majorFlag = flagSet.Int("major", -1, "Major number of the block device")
 	minorFlag = flagSet.Int("minor", -1, "Minor number of the block device")
 	typeFlag  = flagSet.String("type", "", "Type of the node to create (i.e. socket, link, regular, block, directory, character, fifo)")
-	modeFlag  = flagSet.Int("mode", 0644, "Mode to create the node with")
+	modeFlag  = flagSet.String("mode", "644", "Mode (in octal) to create the node with")
 	helpFlag  = flagSet.Bool("help", false, "Show this help")
 
 	typemap = map[string]uint32{
@@ -46,7 +47,12 @@ func Mknod(call []string) os.Error {
 	if mode == syscall.S_IFBLK && (*majorFlag == -1 || *minorFlag == -1) {
 		return os.NewError("When creating a block device, both minor and major number have to be given")
 	}
-	mode |= uint32(*modeFlag)
+
+	fmode, e := strconv.Btoui64(*modeFlag, 8)
+	if e != nil {
+		return e
+	}
+	mode |= uint32(fmode)
 
 	errno := syscall.Mknod(flagSet.Arg(0), mode, *majorFlag<<16|*minorFlag)
 	if errno != 0 {
