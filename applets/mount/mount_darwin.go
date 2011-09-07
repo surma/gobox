250014c9717365528158e2d80110a7e9456c1110
Alexander Surma <alexander.surma@gmail.com>
2011-09-07 13:56:29 +0200

    Mount: Dummy implementation for darwin

---
+++ applets/mount/mount_darwin.go
@@ -0,0 +1,31 @@
+package mount
+
+import (
+	"flag"
+	"os"
+	//"syscall"
+	//"strings"
+)
+
+var (
+	flagSet   = flag.NewFlagSet("mount", flag.PanicOnError)
+	typeFlag  = flagSet.String("t", "", "Filesystem type of the mount")
+	flagsFlag = flagSet.String("o", "", "Comma-separated list of flags for the mount (ro, noexec, nosuid, nodev, synchronous, remount)")
+	helpFlag  = flagSet.Bool("help", false, "Show this help")
+)
+
+func Mount(call []string) os.Error {
+	/*e := flagSet.Parse(call[1:])
+	if e != nil {
+		return e
+	}
+
+	if flagSet.NArg() != 2 || *helpFlag {
+		println("`mount` [options] <device> <dir>")
+		flagSet.PrintDefaults()
+		return nil
+	}
+
+	return nil*/
+	return os.NewError("`mount` not implemented for darwin")
+}
