9d95d315833a953464957d87fa6e53f59e26abaa
Alexander Surma <alexander.surma@gmail.com>
2011-09-07 18:02:30 +0200

    Chroot: Able to chroot and exec

---
+++ applets/chroot/chroot.go
@@ -0,0 +1,36 @@
+package chroot
+
+import (
+	"flag"
+	"os"
+	"syscall"
+)
+
+var (
+	flagSet  = flag.NewFlagSet("chroot", flag.PanicOnError)
+	helpFlag = flagSet.Bool("help", false, "Show this help")
+)
+
+func Chroot(call []string) os.Error {
+	e := flagSet.Parse(call[1:])
+	if e != nil {
+		return e
+	}
+
+	if flagSet.NArg() < 2 || *helpFlag {
+		println("`chroot` [options] <new root> <command>")
+		flagSet.PrintDefaults()
+		return nil
+	}
+
+	errno := syscall.Chroot(flagSet.Arg(0))
+	if errno != 0 {
+		return os.NewError(syscall.Errstr(errno))
+	}
+
+	errno = syscall.Exec(flagSet.Arg(1), flagSet.Args()[1:], os.Envs)
+	if errno != 0 {
+		return os.NewError(syscall.Errstr(errno))
+	}
+	return nil
+}
