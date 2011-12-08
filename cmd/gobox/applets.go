f60196a58121a3cf75ccb6b887bdf47a9a01ad21
Alexander Surma <alexander.surma@gmail.com>
2011-12-09 00:23:42 +0100

    GoBox: Fixed

--- cmd/gobox/applets.go
+++ cmd/gobox/applets.go
@@ -2,51 +2,51 @@ package main
 
 // Applet imports
 import (
-	"applets/cat"
-	"applets/chroot"
-	"applets/echo"
-	"applets/grep"
-	"applets/gzip"
-	"applets/head"
-	"applets/httpd"
-	"applets/ifconfig"
-	"applets/kill"
-	"applets/ls"
-	"applets/mkdir"
-	"applets/mknod"
-	"applets/mount"
-	"applets/ps"
-	"applets/rm"
-	"applets/shell"
-	"applets/telnetd"
-	"applets/umount"
+	// "applets/cat"
+	// "applets/chroot"
+	// "applets/echo"
+	// "applets/grep"
+	// "applets/gzip"
+	// "applets/head"
+	// "applets/httpd"
+	// "applets/ifconfig"
+	// "applets/kill"
+	// "applets/ls"
+	// "applets/mkdir"
+	// "applets/mknod"
+	// "applets/mount"
+	// "applets/ps"
+	// "applets/rm"
+	// "applets/shell"
+	// "applets/telnetd"
+	// "applets/umount"
 	"applets/wget"
 )
 
 // This map contains the mappings from callname
 // to applet function.
 var Applets map[string]Applet = map[string]Applet{
-	"echo":     echo.Echo,
-	"shell":    shell.Shell,
-	"telnetd":  telnetd.Telnetd,
-	"ls":       ls.Ls,
-	"rm":       rm.Rm,
-	"httpd":    httpd.Httpd,
-	"wget":     wget.Wget,
-	"kill":     kill.Kill,
-	"cat":      cat.Cat,
-	"ifconfig": ifconfig.Ifconfig,
-	"mknod":    mknod.Mknod,
-	"mount":    mount.Mount,
-	"umount":   umount.Umount,
-	"chroot":   chroot.Chroot,
-	"ps":       ps.Ps,
-	"mkdir":    mkdir.Mkdir,
-	"head":     head.Head,
-	"grep":     grep.Grep,
-	"gzip":     gzip.Gzip,
-	"gunzip":   gzip.Gunzip,
-	"zcat":     gzip.Zcat,
+	// "echo":     echo.Echo,
+	// "shell":    shell.Shell,
+	// "telnetd":  telnetd.Telnetd,
+	// "ls":       ls.Ls,
+	// "rm":       rm.Rm,
+	// "httpd":    httpd.Httpd,
+	"wget":     wget.Main,
+	// "kill":     kill.Kill,
+	// "cat":      cat.Cat,
+	// "ifconfig": ifconfig.Ifconfig,
+	// "mknod":    mknod.Mknod,
+	// "mount":    mount.Mount,
+	// "umount":   umount.Umount,
+	// "chroot":   chroot.Chroot,
+	// "ps":       ps.Ps,
+	// "mkdir":    mkdir.Mkdir,
+	// "head":     head.Head,
+	// "grep":     grep.Grep,
+	// "gzip":     gzip.Gzip,
+	// "gunzip":   gzip.Gunzip,
+	// "zcat":     gzip.Zcat,
 }
 
 // Signature of applet functions.
@@ -54,4 +54,4 @@ var Applets map[string]Applet = map[string]Applet{
 // name of the applet itself in call[0].
 // If the returned error is not nil, it is printed
 // to stdout.
-type Applet func(call []string) error
+type Applet func()
