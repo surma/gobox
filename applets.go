b12ca242fe549e28e27319b2f0f94fa71795a159
Stefan Schroeder <ondekoza@gmail.com>
2015-01-15 10:26:19 +0100

    Added date-applet.

--- applets.go
+++ applets.go
@@ -4,6 +4,7 @@ package main
 import (
 	"github.com/surma/gobox/applets/cat"
 	"github.com/surma/gobox/applets/chroot"
+	"github.com/surma/gobox/applets/date"
 	"github.com/surma/gobox/applets/echo"
 	"github.com/surma/gobox/applets/grep"
 	"github.com/surma/gobox/applets/gzip"
@@ -45,6 +46,7 @@ var Applets map[string]Applet = map[string]Applet{
 	"gzip":    gzip.Gzip,
 	"gunzip":  gzip.Gunzip,
 	"zcat":    gzip.Zcat,
+	"date":    date.Date,
 }
 
 // Signature of applet functions.
