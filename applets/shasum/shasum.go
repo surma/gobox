fbc0f481e872268ecf3f118176b828aa7685ee8d
atakanyenel <atakanyenel@gmail.com>
2018-02-16 12:51:49 +0100

    remove repeated code

--- applets/shasum/shasum.go
+++ applets/shasum/shasum.go
@@ -6,6 +6,7 @@ import (
 	"crypto/sha512"
 	"flag"
 	"fmt"
+	"hash"
 	"io"
 	"os"
 )
@@ -17,50 +18,43 @@ var (
 
 //Sha1sum calculates the checksum of file with sha1 hashing algorithm
 func Sha1sum(call []string) error {
-	e := flagSet.Parse(call[1:])
-	if e != nil {
+	if e := showHelp(call[1:], "sha1sum"); e != nil {
 		return e
-	}
 
-	if flagSet.NArg() <= 0 || *helpFlag {
-		println("`sha1sum` <files>")
-		flagSet.PrintDefaults()
-		return nil
 	}
-	for _, file := range flagSet.Args() {
-		f, err := os.Open(file)
-		if err != nil {
-			return err
-		}
-		defer f.Close()
-		h := sha1.New()
-		if _, err := io.Copy(h, f); err != nil {
-			return err
-		}
-		fmt.Printf("%x %v\n", h.Sum(nil), file)
-	}
-	return nil
+	h := sha1.New()
+	return hashFiles(h)
+
 }
 
 //Sha256sum calculates the checksum of file with sha256 hashing algorithm
 func Sha256sum(call []string) error {
-	e := flagSet.Parse(call[1:])
-	if e != nil {
+	if e := showHelp(call[1:], "sha256sum"); e != nil {
+		return e
+
+	}
+	h := sha256.New()
+	return hashFiles(h)
+
+}
+
+//Sha512sum calculates the checksum of file with sha512 hashing algorithm
+func Sha512sum(call []string) error {
+	if e := showHelp(call[1:], "sha512sum"); e != nil {
 		return e
 	}
+	h := sha512.New()
+	return hashFiles(h)
 
-	if flagSet.NArg() <= 0 || *helpFlag {
-		println("`sha256sum` <files>")
-		flagSet.PrintDefaults()
-		return nil
-	}
+}
+
+func hashFiles(h hash.Hash) error {
 	for _, file := range flagSet.Args() {
 		f, err := os.Open(file)
 		if err != nil {
 			return err
 		}
 		defer f.Close()
-		h := sha256.New()
 		if _, err := io.Copy(h, f); err != nil {
 			return err
 		}
@@ -69,29 +63,17 @@ func Sha256sum(call []string) error {
 	return nil
 }
 
-//Sha512sum calculates the checksum of file with sha512 hashing algorithm
-func Sha512sum(call []string) error {
-	e := flagSet.Parse(call[1:])
+func showHelp(call []string, commandName string) error {
+	e := flagSet.Parse(call)
 	if e != nil {
 		return e
 	}
 
 	if flagSet.NArg() <= 0 || *helpFlag {
-		println("`sha512sum` <files>")
+		println(commandName, " <files>")
 		flagSet.PrintDefaults()
 		return nil
 	}
-	for _, file := range flagSet.Args() {
-		f, err := os.Open(file)
-		if err != nil {
-			return err
-		}
-		defer f.Close()
-		h := sha512.New()
-		if _, err := io.Copy(h, f); err != nil {
-			return err
-		}
-		fmt.Printf("%x %v\n", h.Sum(nil), file)
-	}
+
 	return nil
 }
