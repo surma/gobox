fa88519b8c1c8648d811dd4244a7c6fce47bf90c
atakanyenel <atakanyenel@gmail.com>
2018-02-19 16:19:43 +0100

    add help to pwd

--- applets/pwd/pwd.go
+++ applets/pwd/pwd.go
@@ -1,10 +1,33 @@
 package pwd
 
 import (
+	"flag"
+	"fmt"
 	"os"
 )
 
+var (
+	flagSet  = flag.NewFlagSet("pwd", flag.PanicOnError)
+	helpFlag = flagSet.Bool("help", false, "Show this help")
+)
+
+//Pwd outputs the current working directory
 func Pwd(call []string) error {
+	e := flagSet.Parse(call[1:])
+	if e != nil {
+		return e
+	}
+
+	if flagSet.NArg() > 0 {
+		fmt.Println("Expected 0 args, got 1")
+		return nil
+
+	}
+	if *helpFlag {
+		println("`pwd`")
+		flagSet.PrintDefaults()
+		return nil
+	}
 
 	pwd, _ := os.Getwd()
 	println(pwd)
